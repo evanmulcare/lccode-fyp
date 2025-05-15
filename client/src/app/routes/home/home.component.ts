4043360ac9f30cde1905bf7bc8b4c78cbcb75580
Evan Mulcare R00211686 <evanmulcare@gmail.com>
2025-05-15 10:07:43 +0100

    update

--- client/src/app/routes/home/home.component.ts
+++ client/src/app/routes/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  openDocumentation() {
-    alert('No Documentation Added');
+  openDocumentation(link: string): void {
+    window.open(link, '_blank');
   }
 }
